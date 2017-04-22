3a2ab50641c7fd1caf41476e708fd8b84518b9c4
Alejandro Reyes Bautista <reyes.bautista.alejandro@gmail.com>
2017-04-22 18:08:24 +0100

    User can add a new point of interest (POI) clicking in the map. A separate activity will be opened with fields for name, type and description. Types are predefined, so we use a spinner (drop down list) whose values are defined in resources>strings. If any field is empty we display an error. The POI appears on the map as a marker as soon as it has been added.

--- app/src/main/java/xyz/alejandoreba/pointsofinterestapp/MainActivity.java
+++ app/src/main/java/xyz/alejandoreba/pointsofinterestapp/MainActivity.java
@@ -3,6 +3,7 @@ package xyz.alejandoreba.pointsofinterestapp;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,12 +12,20 @@ import android.preference.PreferenceManager;
 import android.widget.Toast;
 
 import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements LocationListener {
 
     MapView mv;
+    ItemizedIconOverlay<OverlayItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +38,47 @@ public class MainActivity extends Activity implements LocationListener {
         setContentView(R.layout.activity_main);
         mv = (MapView) findViewById(R.id.map1);
 
+
+        //initialize the list of items (POIs) over the map
+         this.items = new ItemizedIconOverlay<OverlayItem>(this, new ArrayList<OverlayItem>(), null);
+
+        //listener for tap clicks on the map
+        //when you click you intend the new activity to add a POI
+        MapEventsReceiver eventsReceiver = new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                /*
+                Toast.makeText (MainActivity.this, "singleTapConfirmedHelper go go go -> lat:" + p.getLatitude() + " , long: " + p.getLongitude(), Toast.LENGTH_LONG).show();
+                return false;
+                */
+
+                Intent intent = new Intent(MainActivity.this, addPOI.class);
+                Bundle bundle = new Bundle();
+                bundle.putDouble("poilat", p.getLatitude());
+                bundle.putDouble("poilon", p.getLongitude());
+                intent.putExtras(bundle);
+                startActivityForResult(intent, 0);
+
+                return false;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                Toast.makeText (MainActivity.this, "longPressHelper go go go", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        };
+
+        Overlay eventsOnMap = new MapEventsOverlay(eventsReceiver);
+        mv.getOverlays().add(eventsOnMap);
+        //
+
+        //center and zoom the map
         mv.getController().setZoom(14);
         mv.setBuiltInZoomControls(true);
         mv.getController().setCenter(new GeoPoint(50.9319, -1.4011));
 
+        //check GPS service is on
         LocationManager mgr=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
         try {
             mgr.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
@@ -66,6 +112,39 @@ public class MainActivity extends Activity implements LocationListener {
     public void onProviderDisabled(String provider) {
         Toast.makeText(this, "Provider " + provider +
                 " disabled", Toast.LENGTH_LONG).show();
+    }
+
+    //method to manage results of activities.
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                String name = extras.getString("poiname");
+                String type = extras.getString("poitype");
+                String description = extras.getString("poidescription");
+                double lat = extras.getDouble("poilat");
+                double lon = extras.getDouble("poilon");
+
+
+                Toast.makeText(this, "POI virtually added -> name: " + name + ", type: " + type +
+                        ", lat: " + lat + ", lon: " + lon + " description: " + description,
+                        Toast.LENGTH_LONG).show();
+
+
+                OverlayItem newItem = new OverlayItem(name,type,description, new GeoPoint(lat,lon));
+                items.addItem(newItem);
+                mv.getOverlays().add(items);
+                //refresh the map in order to show the new POI immediately
+                mv.invalidate();
+
+            }
+
+        }
     }
 }
 
---
+++ app/src/main/java/xyz/alejandoreba/pointsofinterestapp/addPOI.java
@@ -0,0 +1,72 @@
+package xyz.alejandoreba.pointsofinterestapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class addPOI extends Activity implements View.OnClickListener {
+    double lat;
+    double lon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_poi);
+
+        Button bAdd = (Button) findViewById(R.id.btnAddPoi);
+        bAdd.setOnClickListener(this);
+        Button bBack = (Button) findViewById(R.id.btnBackAddPoi);
+        bBack.setOnClickListener(this);
+
+        Bundle bundle = getIntent().getExtras();
+        this.lat = bundle.getDouble("poilat");
+        this.lon = bundle.getDouble("poilon");
+
+        ((EditText) findViewById(R.id.poilat)).setText(String.valueOf(lat));
+        ((EditText) findViewById(R.id.poilon)).setText(String.valueOf(lon));
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.btnAddPoi) {
+            Intent intent = new Intent();
+
+            Bundle bundle = new Bundle();
+            EditText name = (EditText) findViewById(R.id.poiname);
+            EditText description = (EditText) findViewById(R.id.poidescription);
+
+            if ((name.getText().toString()).trim().equals("") || (description.getText().toString()).trim().equals("")){
+                TextView error = (TextView) findViewById(R.id.addpoierror);
+                error.setText("ERROR: Please fill both name and description fields");
+            }else {
+
+                bundle.putString("poiname", name.getText().toString().trim());
+                bundle.putString("poidescription", description.getText().toString().trim());
+
+                Spinner spinner = (Spinner) findViewById(R.id.poitype);
+                String type = spinner.getSelectedItem().toString();
+                bundle.putString("poitype", type);
+
+                bundle.putDouble("poilat", this.lat);
+                bundle.putDouble("poilon", this.lon);
+
+                intent.putExtras(bundle);
+
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+
+        }else{
+            setResult(RESULT_CANCELED);
+            finish();
+        }
+    }
+}
